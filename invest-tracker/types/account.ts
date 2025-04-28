73aa7ab5b27de8d93e0a716d4e2fe2727f57be5d
GuiDev <guilherme.alves1amo@gmail.com>
2025-04-27 22:06:12 -0300

    deletando

--- invest-tracker/types/account.ts
+++
@@ -1,8 +0,0 @@
-export interface Account {
-  id: string;
-  userId: string;
-  userName: string;
-  name: string;
-  balance: number;
-  // Adicione outras propriedades da conta conforme necessário
-}
--- invest-tracker/types/user.ts
+++
@@ -1,6 +0,0 @@
-export interface User {
-  userId: string;
-  username: string;
-  email: string;
-  password: string;
-}
